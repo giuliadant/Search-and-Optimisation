d466dce0afea5e65d7ff84a00ef3974e316d4249
giuliadant <giulia.dant2003@gmail.com>
2025-05-03 14:05:22 +0200

    updated code

--- Q1.py
+++ Q1.py
@@ -13,7 +13,7 @@ def part_dev_G(x):
     return np.array([dx, dy])
 
 N = 50
-alpha = 0.2
+alpha = 0.4
 all_Gx = np.zeros((100,N)) #Stores values of G(x) for each run, for each iteration
 final_values = [] #list to store final value of G(x) from each of the 100 runs
 final_points = [] #list to store final values of x from each run
@@ -30,6 +30,7 @@ for run in range(100): #run gradient for 100 times, each time starting with a ra
 
 # Compute average G(x) at each iteration across all runs
 average_Gx = np.mean(all_Gx, axis=0)
+average_final_Gx = np.mean(final_values)
 
 # Find the index of the best final G(x) across all 100 runs
 best_index = np.argmin(final_values)
@@ -42,12 +43,15 @@ best_x = final_points[best_index]
 plt.plot(range(N), average_Gx, 'yo-')
 plt.xlabel('Iterations')
 plt.ylabel('G(x)')
-plt.title('Average G(x) for 100 Gradient Descent Runs')
+plt.title('Average G(x) for Gradient Descent')
 plt.grid()
 plt.show()
 
 # Compute and print std deviation of final values
 std_dev = np.std(final_values)
 print(f"Standard Deviation of final G(x) values: {std_dev}")
-print(f"Best G(x) found after 100 runs:{best_gx}")   # best G(x) found
+print(f"Average final G(x) across 100 runs: {average_final_Gx}")
+print(f"Best G(x) found after 100 runs:{best_gx}")
 print(f"Minimizer x at = {best_x}")
+
+
