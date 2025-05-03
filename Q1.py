735d93a653872c46aed1c3136c0e77cc0e83c89f
giuliadant <giulia.dant2003@gmail.com>
2025-05-03 12:27:01 +0200

    Gradient Descent updated code

--- Q1.py
+++ Q1.py
@@ -12,34 +12,42 @@ def part_dev_G(x):
     dy = ((2 * x[1]) / 4000 + const * np.cos(x[0]) * np.sin(const * x[1]))
     return np.array([dx, dy])
 
-N = 15
-all_Gx = np.zeros((100,N))
-final_values = []
+N = 50
+alpha = 0.2
+all_Gx = np.zeros((100,N)) #Stores values of G(x) for each run, for each iteration
+final_values = [] #list to store final value of G(x) from each of the 100 runs
+final_points = [] #list to store final values of x from each run
 
-for run in range(100):
+for run in range(100): #run gradient for 100 times, each time starting with a random point
+    x_i = np.random.uniform(-10, 10, size=2)
     # gradient descent
-    x1 = np.random.uniform(-10, 10)
-    x2 = np.random.uniform(-10, 10)
-    x_i = ([x1, x2])
-    alpha = 0.3
-    epsilon = 0.01
     for i in range(N):
-        grad = -part_dev_G(x_i)
-        x_i = x_i + alpha * grad
-        all_Gx[run,i] = G(x_i)
-    final_values.append(all_Gx[run,-1])
+        gradient = part_dev_G(x_i)
+        x_i = x_i - alpha * gradient
+        all_Gx[run,i] = G(x_i) #evaluates function at current iteration in run number
+    final_values.append(all_Gx[run,-1]) #function value at last iteration of run
+    final_points.append(x_i.copy())  # store the minimizer
 
 # Compute average G(x) at each iteration across all runs
 average_Gx = np.mean(all_Gx, axis=0)
 
+# Find the index of the best final G(x) across all 100 runs
+best_index = np.argmin(final_values)
+
+# Get the best G(x) and the corresponding minimizer x
+best_gx = final_values[best_index]
+best_x = final_points[best_index]
+
 # Plot average G(x) over time
-plt.plot(range(N), average_Gx)
-plt.xlabel('Iteration')
-plt.ylabel('Average G(x)')
-plt.title('Average G(x) over 100 Gradient Descent Runs')
+plt.plot(range(N), average_Gx, 'yo-')
+plt.xlabel('Iterations')
+plt.ylabel('G(x)')
+plt.title('Average G(x) for 100 Gradient Descent Runs')
 plt.grid()
 plt.show()
 
 # Compute and print std deviation of final values
 std_dev = np.std(final_values)
 print(f"Standard Deviation of final G(x) values: {std_dev}")
+print(f"Best G(x) found after 100 runs:{best_gx}")   # best G(x) found
+print(f"Minimizer x at = {best_x}")
